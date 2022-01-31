636be403fec0655c49ee2d6a157514dbce69696a
chihiro-kakizaki <kkzkchr@gmail.com>
2022-01-31 21:43:37 +0900

    データベース作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_121905) do
+ActiveRecord::Schema.define(version: 2022_01_31_124015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
   end
 
 end
